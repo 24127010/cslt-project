44ad0f6ff686118e64b48dcdc5421ac37005b3cf
Wili-102 <vanhuuphuc102@gmail.com>
2026-05-11 21:05:52 +0700

    hoan_thanh_GameBotMedium.h

---
+++ src/GameBotMedium.h
@@ -0,0 +1,12 @@
+// GameBotMedium.h
+#pragma once
+#include "BaseGameMode.h"
+
+class MediumMode : public BaseGameMode {
+public:
+    MediumMode(sf::RenderWindow& win);
+
+    void aiControlBot(float dt) override;
+    void update(float dt) override;   // ? implement
+    void render() override;           // ? implement
+}; 
